62ead8abe380702004838f77041061fd5455dc0b
charlotte <chafred38@gmail.com>
2020-12-14 11:40:46 +0100

    Signed-off-by: charlotte <chafred38@gmail.com>premier commit

---
+++ essai.js
@@ -0,0 +1,19 @@
+function charlotte() {
+
+	var maVariable = 10;
+
+alert (maVariable);
+}
+
+console.log(2+2);
+//exped result: 4
+
+document.getElementByID("lien").addventListener('contextmenu', fuction(e) ){ 
+	id (prompt("ouverture du menu, confirmez : ;" ) ) 
+	 });
+
+document.getElementByID("inscription").addventListener(submit, fonction()
+	{
+	alert ('formualaire envoyé!');
+	 
+	});
